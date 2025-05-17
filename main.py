f7cfe40bce39ce441385594913952135be57a10a
janeckiisa <janeckiisabelle@gmail.com>
2025-05-16 21:21:34 -0300

    Testing

---
+++ main.py
@@ -0,0 +1,9 @@
+from functions.binary import *
+
+numero = int(input("Insira o número a ser codificado: \n> "))
+resultado = numeroParaBinario(numero)
+
+# binario = input("Insira o binário: \n> ")
+# resultado = binarioParaNumero(binario)
+
+print(resultado)
